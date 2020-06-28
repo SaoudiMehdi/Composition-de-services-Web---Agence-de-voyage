5223749c39bf14cc50c29cdc4f084f356587d0b3
younessmouad <ymouad96@gmail.com>
2020-06-28 10:21:28 +0100

    Hibernate

---
+++ AgencyOffersBeta/src/main/java/utility/HibernateUtil.java
@@ -0,0 +1,116 @@
+package utility;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import Beans.Car;
+
+
+
+public class HibernateUtil {
+
+    //XML based configuration
+    private static SessionFactory sessionFactory;
+
+    //Annotation based configuration
+    private static SessionFactory sessionAnnotationFactory;
+
+    //Property based configuration
+    private static SessionFactory sessionJavaConfigFactory;
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("Hibernate.cfg.xml");
+            System.out.println("Hibernate Configuration loaded");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate serviceRegistry created");
+
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            System.out.println(sessionFactory);
+            return sessionFactory;
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static SessionFactory buildSessionAnnotationFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate-annotation.cfg.xml");
+            System.out.println("Hibernate Annotation Configuration loaded");
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate Annotation serviceRegistry created");
+
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            return sessionFactory;
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static SessionFactory buildSessionJavaConfigFactory() {
+        try {
+            Configuration configuration = new Configuration();
+
+            //Create Properties, can be read from property files too
+            Properties props = new Properties();
+            props.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            props.put("hibernate.connection.url", "jdbc:mysql://sql7.freemysqlhosting.net/sql7349825?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+            props.put("hibernate.connection.username", "sql7349825");
+            props.put("hibernate.connection.password", "WieEEaFh3j");
+            props.put("hibernate.current_session_context_class", "thread");
+
+            configuration.setProperties(props);
+
+            //we can set mapping file or class with annotation
+            //addClass(Employee1.class) will look for resource
+            // com/journaldev/hibernate/model/Employee1.hbm.xml (not good)
+            configuration.addAnnotatedClass(Car.class);
+            //configuration.addAnnotatedClass(comite.class);
+
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate Java Config serviceRegistry created");
+
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            System.out.println(sessionFactory);
+            return sessionFactory;
+        }
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) sessionFactory = buildSessionFactory();
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionAnnotationFactory() {
+        if(sessionAnnotationFactory == null) sessionAnnotationFactory = buildSessionAnnotationFactory();
+        return sessionAnnotationFactory;
+    }
+
+    public static SessionFactory getSessionJavaConfigFactory() {
+        if(sessionJavaConfigFactory == null) sessionJavaConfigFactory = buildSessionJavaConfigFactory();
+        return sessionJavaConfigFactory;
+    }
+
+}
